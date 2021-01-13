903fcba62a0cb425ae9ddaec7882c5530e74153f
Jordin Gardner <gard.jordin@gmail.com>
2021-01-13 14:19:10 -0700

    fix misnamed env var

--- src/js/utils/Debug.ts
+++ src/js/utils/Debug.ts
@@ -1,7 +1,7 @@
 export default {
   log: (...args: any[]) => {
     const value = args.join(", ");
-    if (process.env.NODE_ENV !== "prod") {
+    if (process.env.NODE_ENV !== "production") {
       console.log(value);
     }
     return value;
